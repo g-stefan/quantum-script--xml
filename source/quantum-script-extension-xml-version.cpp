fd94f39d0cd60e303f52b5aae8f8b63f2afc4c9d
Grigore Stefan <g_stefan@yahoo.com>
2022-01-12 00:58:14 +0200

    Update

--- source/quantum-script-extension-xml-version.cpp
+++ source/quantum-script-extension-xml-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace XML {
 				namespace Version {
 
-					static const char *version_ = "2.7.0";
-					static const char *build_ = "21";
-					static const char *versionWithBuild_ = "2.7.0.21";
-					static const char *datetime_ = "2022-01-01 21:19:44";
+					static const char *version_ = "2.12.0";
+					static const char *build_ = "24";
+					static const char *versionWithBuild_ = "2.12.0.24";
+					static const char *datetime_ = "2022-01-09 01:13:14";
 
 					const char *version() {
 						return version_;
